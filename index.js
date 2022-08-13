2e5303eb04e5429712f36ed99f70fb81bbf0a05a
Anubhav Jain <anubhav823@gmail.com>
2022-08-13 14:49:22 +0530

    Added styling and translators for different languages

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
 // let promptAnswer = prompt('I am prompting you for an input')
 // alert(promptAnswer)
 
-let button = document.querySelector('#button-ex4')
+let button_yoda = document.querySelector('#button-yoda')
+let button_minionese = document.querySelector('#button-minionese')
+let button_vulcan = document.querySelector('#button-vulcan')
+let button_dothraki = document.querySelector('#button-dothraki')
 let textarea = document.querySelector('#textarea-ex4')
-let outputtextarea = document.querySelector('#textarea-ex4-1')
+let outputtextarea = document.querySelector('#outputtextarea')
 
-let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
-button.addEventListener('click', () => {
+let url = "https://api.funtranslations.com/translate/"
+button_yoda.addEventListener('click', () => {
     let text = textarea.value;
-    let test = constructURL(text)
+    let test = constructURL('yoda',text)
+    console.log(test);
     event.preventDefault();
     fetchURL(test)
 })
 
-function constructURL(text) {
-    return url + text;
+button_minionese.addEventListener('click', () => {
+    let text = textarea.value;
+    let test = constructURL('minion', text)
+    console.log(test);
+    event.preventDefault();
+    fetchURL(test)
+})
+
+button_vulcan.addEventListener('click', () => {
+    let text = textarea.value;
+    let test = constructURL('vulcan',text)
+    console.log(test);
+    event.preventDefault();
+    fetchURL(test)
+})
+
+button_dothraki.addEventListener('click', () => {
+    let text = textarea.value;
+    let test = constructURL('dothraki',text)
+    console.log(test);
+    event.preventDefault();
+    fetchURL(test)
+})
+
+function constructURL(language, text) {
+    return url + language + '.json?text=' + text;
 }
 
 function fetchURL(test) {
     fetch(test)
         .then((response) => response.json())
         .then(json => {
-            let translatedText = json.contents.text;
-            outputtextarea.innerText = translatedText;
+            let translatedText = json.contents.translated;
+            let textNode = document.createElement("p");
+            while (outputtextarea.firstChild) {
+                outputtextarea.removeChild(outputtextarea.firstChild);
+            }
+            textNode.innerText = translatedText;
+            outputtextarea.appendChild(textNode)
         })
-        .catch((err) => errorHandler(err))
+        .catch(errorHandler)
 }
 
-function errorHandler(err) {
-    console.log(err)
+function errorHandler() {
+    while (outputtextarea.firstChild) {
+        outputtextarea.removeChild(outputtextarea.firstChild);
+    }
+    let textNode = document.createElement("p");
+    textNode.innerText = "Something went wrong. Please try again later."
+    outputtextarea.appendChild(textNode)
 }
